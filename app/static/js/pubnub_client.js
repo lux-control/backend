88d47bdb3b025688cb1bd732201a22b6d037c413
Dmytro Bokov <dimabokov5@gmail.com>
2026-01-11 20:11:59 +0000

    feat: make realtime lux display

--- app/static/js/pubnub_client.js
+++ app/static/js/pubnub_client.js
@@ -1,9 +1,14 @@
+let lux = 0
+
 const pubnub = new PubNub({
     publishKey: "pub-c-1917ca54-c901-4ec2-a145-b0f197c3882e",
     subscribeKey: "sub-c-c5848d3f-0fce-45dd-b398-9147c61b581f",
     userId: "web-pi-light-controller" + Math.floor(Math.random() * 1000)
 });
 
+pubnub.subscribe({
+    channels: ["iot.status"]
+});
 
 function publishMessage(payload) {
     pubnub.publish({
@@ -17,3 +22,22 @@ function publishMessage(payload) {
         console.error("Publish failed:", err);
     });
 }
+
+pubnub.addListener({
+    message: function(event) {
+        msg = event.message
+        console.log("New message:", msg);
+        lux = msg.lux
+        console.log("Lux: " + lux)
+
+        span = document.getElementById("lux-indicator").innerHTML = lux
+    },
+    status: function(event) {
+        console.log("Status event:", event);
+        if (event.category === "PNConnectedCategory") {
+            console.log("Connected to PubNub channels!");
+        } else if (event.category === "PNNetworkIssuesCategory") {
+            console.log("Connection lost. Attempting to reconnect...");
+        }
+    }
+});
